35e74ee430e446b649b0aff0a6567bde9d11d4e7
babytayo <121583836+babytayo@users.noreply.github.com>
2023-04-23 19:54:57 +0100

    under construction

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// let menu = false;
+
+// function onClickMenu() {
+//   console.log("BEFORE", menu);
+//   menu = !menu;
+//   console.log("AFTER", menu);
+//   //   document.getElementById("menu").style.display = block;
+// }
+
+// // now you can call your function to toogle the MENU
+// // onClickMenu();
+// menu.toggle()
+function onClickMenu() {
+  document.getElementById("menu").style.display = "block";
+}
