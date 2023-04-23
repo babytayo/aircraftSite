f83b96c45cbe11e5262d5572fbc5901c64421383
babytayo <121583836+babytayo@users.noreply.github.com>
2023-04-23 21:57:56 +0100

    added some js functions

--- js/index.js
+++ js/index.js
@@ -1,15 +1,19 @@
-// let menu = false;
+let menu = false;
 
-// function onClickMenu() {
-//   console.log("BEFORE", menu);
-//   menu = !menu;
-//   console.log("AFTER", menu);
-//   //   document.getElementById("menu").style.display = block;
-// }
-
-// // now you can call your function to toogle the MENU
-// // onClickMenu();
-// menu.toggle()
 function onClickMenu() {
-  document.getElementById("menu").style.display = "block";
+  console.log("BEFORE", menu);
+  menu = !menu;
+  console.log("AFTER", menu);
+}
+
+// now you can call your function to toogle the MENU
+// onClickMenu();
+
+function onClickMenu() {
+  var nav = document.getElementById("menu");
+  if (nav.style.display == "block") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "block";
+  }
 }
