4bcdcd92bb32d6079599586963491f2532bd2ccd
babytayo <121583836+babytayo@users.noreply.github.com>
2023-04-26 21:26:16 +0100

    js statements updated

--- js/index.js
+++ js/index.js
@@ -1,19 +1,24 @@
-let menu = false;
-
-function onClickMenu() {
-  console.log("BEFORE", menu);
-  menu = !menu;
-  console.log("AFTER", menu);
-}
-
-// now you can call your function to toogle the MENU
-// onClickMenu();
-
+//Hamburger toggling Statement
 function onClickMenu() {
   var nav = document.getElementById("menu");
   if (nav.style.display == "block") {
     nav.style.display = "none";
+    console.log("hideHamburgerMenu");
   } else {
     nav.style.display = "block";
+    console.log("displayHamburgerMenu");
+  }
+}
+
+//Changing img src toggle Statement
+
+function swapImg() {
+  var img = document.getElementById("image");
+  if (img.src.match("./icons8-menu-rounded-30.png")) {
+    img.src = "././image/cancel.png";
+    console.log("otherImg");
+  } else {
+    img.src = "././image/icons8-menu-rounded-30.png";
+    console.log("mainImg");
   }
 }
